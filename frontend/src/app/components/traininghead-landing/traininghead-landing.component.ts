66eb88f71f765283457f456c3cd697311296bbe0
vaishak-krishnan <vaishak.krishnan@gmail.com>
2022-12-13 18:34:30 +0530

    first commit

---
+++ frontend/src/app/components/traininghead-landing/traininghead-landing.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Router } from '@angular/router';
+
+// fontawesome
+import {faFile} from '@fortawesome/free-regular-svg-icons'
+import {faPlusSquare} from '@fortawesome/free-regular-svg-icons'
+import {faEdit} from '@fortawesome/free-regular-svg-icons'
+import {faWindowClose} from '@fortawesome/free-regular-svg-icons'
+
+@Component({
+  selector: 'app-traininghead-landing',
+  templateUrl: './traininghead-landing.component.html',
+  styleUrls: ['./traininghead-landing.component.css']
+})
+export class TrainingheadLandingComponent implements OnInit {
+
+  students : any = [];
+  id:any;
+  searchText:any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 6;
+  filterTerm!: string;
+
+    // fontawesome
+    faFile = faFile;
+    faPlusSquare = faPlusSquare;
+    faWindowClose = faWindowClose;
+    faEdit = faEdit;
+
+  constructor(private apiService: ApiService,  private router: Router) { }
+
+  ngOnInit(): void {
+    this.getstudentData();
+
+  }
+
+  
+  getstudentData() {
+    this.apiService.getstudentsList().subscribe(res => {
+      this.students = res;
+    })
+  }
+
+  updatestudent(_id: any) {
+    console.log(_id)
+    this.apiService.formupdate = _id;
+    console.log(_id)
+    this.router.navigate(['/traininghead-edit-students']);
+  }
+
+  deletestudent(id:any){
+    this.apiService.deletestudent(id).subscribe(res=>{
+      this.getstudentData()
+    })
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.getstudentData();
+  } 
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.getstudentData();
+  }
+}
---
+++ frontend/src/app/components/user-add/user-add.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { ApiService } from 'src/app/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {faFile} from '@fortawesome/free-regular-svg-icons'
+import {faPlusSquare} from '@fortawesome/free-regular-svg-icons'
+import {faEdit} from '@fortawesome/free-regular-svg-icons'
+import {faWindowClose} from '@fortawesome/free-regular-svg-icons'
+
+@Component({
+  selector: 'app-user-add',
+  templateUrl: './user-add.component.html',
+  styleUrls: ['./user-add.component.css']
+})
+export class UserAddComponent implements OnInit {
+
+  courses : any = [];
+  users : any = [];
+
+  // fontawesome
+  faFile = faFile;
+  faPlusSquare = faPlusSquare;
+  faWindowClose = faWindowClose;
+  faEdit = faEdit;
+  
+
+  constructor(  private apiService: ApiService,
+    private router:Router,
+    private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getcourseData();
+
+  }
+
+  adduserform: any=  new FormGroup({
+    user_name: new FormControl('', Validators.required),
+    contact_number: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    email_id: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    password: new FormControl("", [Validators.required]),
+    course: new FormControl("", [Validators.required]),
+    office_location: new FormControl("", [Validators.required]),
+    user_type: new FormControl("", [Validators.required])
+  })
+
+
+  // function for get course data to selctbox
+
+  getcourseData() {
+    this.apiService.getcourseList().subscribe(res => {
+      this.courses = res
+  
+      console.log(this.courses)
+    })
+  }
+
+  adduser(){
+    this.apiService.addnewuser(this.adduserform.value).subscribe(res => {
+      if (res) {
+        alert("Data saved successfully");
+        this.router.navigate(['/user-list'])
+
+        // console.log("incoming data from addbook", this.addbookform.value);
+      }
+      else{
+        alert("Data not saved");
+      }
+    })
+  }
+
+}
